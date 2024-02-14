de1cda2d3b36987bd5a68b626205349a73c7e051
Lucca <lucca.2001@hotmail.com>
2024-02-14 02:50:44 -0300

    finalizacao da secao de exibicao de uma enquete, front praticamente pronto

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ export function App() {
 
 
   return(
-    <div className="mx-auto max-w-screen-2xl h-screen flex">
+    <div className="mx-auto max-w-screen-2xl h-screen flex overflow-hidden">
       <div className="min-w-96 shadow-md shadow-stone-300">
         <Create />
       </div>
